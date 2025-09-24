7cd53b51c5b4514a59dbb1ad12dd229361a83554
Aspecky <tarrihmoad@proton.me>
2025-09-24 02:08:34 +0100

    added tilde expantion

--- source/src/parsing/expantion/tilde_expantion.c
+++ source/src/parsing/expantion/tilde_expantion.c
@@ -6,23 +6,27 @@
 /*   By: mtarrih <mtarrih@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 23:56:40 by mtarrih           #+#    #+#             */
-/*   Updated: 2025/09/23 22:18:00 by mtarrih          ###   ########.fr       */
+/*   Updated: 2025/09/24 02:00:11 by mtarrih          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Vector.h"
+#include "ft_stdlib.h"
 #include "ft_string.h"
 #include "parsing.h"
-#include <stdio.h>
 
 char *tilde_expantion(char *str, char *const envp[])
 {
-	t_vector vector;
+	char *value;
+	char *ret;
+
 	if (!(str[0] == '~' && (str[1] == '/' || !str[1])))
 		return (ft_strdup(str));
-
-	// vector_init(&vector, 0, sizeof(char));
-	printf("tilde: '%s'\n", str);
-	return (ft_strdup(str));
-	return (vector.arr);
+	value = getenv_r("HOME", envp);
+	if (!value)
+		return (ft_strdup(str));
+	if (!str[1])
+		ret = ft_strdup(value);
+	else
+		ret = ft_strjoin(value, str + 1);
+	return (ret);
 }
