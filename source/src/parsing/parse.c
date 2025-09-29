e7a0894acb28eaac6d386362e6ff82a9e464b935
Aspecky <tarrihmoad@proton.me>
2025-09-29 19:18:45 +0100

    Handle SIGINT in heredoc + fixed some fd leaks + norminette

--- source/src/parsing/parse.c
+++ source/src/parsing/parse.c
@@ -6,21 +6,25 @@
 /*   By: mtarrih <mtarrih@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 15:13:46 by mtarrih           #+#    #+#             */
-/*   Updated: 2025/09/28 16:24:16 by mtarrih          ###   ########.fr       */
+/*   Updated: 2025/09/29 18:07:39 by mtarrih          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "_debug.h"
+#include "minishell.h"
 #include "parsing.h"
 #include "utils.h"
 #include <errno.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 
 static bool	on_error(t_sllist *tokens, const char *s1, const char *s2)
 {
+	g_last_exit_status = EXIT_FAILURE;
 	sllist_clear(tokens, token_free);
-	print_error(s1, s2);
+	if (s1 && s2)
+		print_error(s1, s2);
 	return (false);
 }
 
@@ -38,11 +42,16 @@ bool	parse(char *stream, char *const envp[], t_sllist *commands)
 	if (!expand_words(&tokens, envp))
 		return (on_error(&tokens, "expantion", strerror(errno)));
 	if (!validate_tokens(&tokens))
-		return (sllist_clear(&tokens, token_free), false);
+		return (on_error(&tokens, 0, 0));
 	remove_quotes(&tokens);
 	if (!tokens_to_commands(commands, &tokens))
 		return (on_error(&tokens, "tokens_to_commands", strerror(errno)));
 	if (!process_heredocs(commands, envp))
-		return (on_error(&tokens, "heredoc", strerror(errno)));
+	{
+		if (errno != EINTR)
+			return (on_error(&tokens, "heredoc", strerror(errno)));
+		else
+			return (on_error(&tokens, 0, 0));
+	}
 	return (sllist_clear(&tokens, token_free), true);
 }
--- source/src/signal_hooks/hook_heredoc_signals.c
+++ source/src/signal_hooks/hook_heredoc_signals.c
@@ -1,41 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   hook_main_signals.c                                :+:      :+:    :+:   */
+/*   hook_heredoc_signals.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mtarrih <mtarrih@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/19 15:46:16 by mtarrih           #+#    #+#             */
-/*   Updated: 2025/09/29 19:11:44 by mtarrih          ###   ########.fr       */
+/*   Created: 2025/09/29 17:47:13 by mtarrih           #+#    #+#             */
+/*   Updated: 2025/09/29 19:15:10 by mtarrih          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "signal_hooks.h"
 #include <readline/readline.h>
-#include <signal.h>
 
 static void	sigint_handler(int sig)
 {
 	(void)sig;
+	heredoc_id(&(int){heredoc_id(0) + 1});
 	printf("\n");
-	if (!is_executing(0))
-	{
-		g_last_exit_status = 1;
-		rl_on_new_line();
-	}
-	rl_replace_line("", false);
-	rl_redisplay();
 }
 
-bool	hook_main_signals(void)
+bool	hook_heredoc_signals(void)
 {
 	struct sigaction	sigint_act;
 
 	init_sigaction(&sigint_act, sigint_handler);
-	sigint_act.sa_flags = SA_RESTART;
-	if (sigaction(SIGINT, &sigint_act, 0) == -1 || signal(SIGQUIT,
-			SIG_IGN) == SIG_ERR)
+	if (sigaction(SIGINT, &sigint_act, 0) == -1)
 		return (false);
 	rl_catch_signals = false;
 	return (true);
